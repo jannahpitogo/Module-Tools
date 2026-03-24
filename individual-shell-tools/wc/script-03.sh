b041cdba57f470a2999240f6f93a2e8c52c44a29
jannahpitogo <pitogojannah@outlook.com>
2026-03-24 20:10:48 +0100

    added -l for lines, -w for words, -c for character and added the folder name plus slash and asterisks to access all files under that folder

--- individual-shell-tools/wc/script-03.sh
+++ individual-shell-tools/wc/script-03.sh
@@ -8,3 +8,6 @@ set -euo pipefail
 #        1       7      39 ../helper-files/helper-2.txt
 #        3      19      92 ../helper-files/helper-3.txt
 #        5      30     151 total
+
+cd .. 
+wc -lwc helper-files/*
