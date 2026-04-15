dbe15a95907909f5b2defe7ce9481b8a32eee167
jannahpitogo <pitogojannah@outlook.com>
2026-04-15 12:17:26 +0200

    fixed

--- implement-shell-tools/cat/cat-n.js
+++ implement-shell-tools/cat/cat-n.js
@@ -1,29 +1,9 @@
 const fs = require("fs");
 
-const args = process.argv.slice(2);
+const filePath = "sample-files/1.txt";
 
-let displayLine = false;
-let data;
+const lines = fs.readFileSync(filePath, "utf-8").split("\n");
 
-if (args[0] === "-n") {
-  displayLine = true;
-  data = args[1]; //script path
-} else {
-  data = args[0]; //node path
-}
-
-try {
-  const content = fs.readFileSync(data, "utf8"); //read the file
-  const lines = content.split("\n"); //splitting it to new line
-
-  lines.forEach((line, index) => {
-    if (displayLine) {
-      const number = String(index + 1).padStart(6);
-      console.log(`${number}\t${line}`);
-    } else {
-      console.log(line);
-    }
-  });
-} catch (err) {
-  console.error(`cat: ${data}: No file available`);
-}
+lines.forEach((line, index) => {
+  console.log(`${index + 1}\t${line}`);
+});
